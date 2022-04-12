d2090459cfca05f850d01065b09e269fa5b8719c
Danilo Melo da Rocha <danilo.melo.rocha@fortnox.se>
2022-04-12 22:22:46 +0200

    Initial work of the views

---
+++ ui/src/views/Lobby/Lobby.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { H1, Row, Column, TextField, PrimaryButton, Validate } from 'theNameThatCanNotBeSaid';
+import { validateRequiredField } from 'theNameThatCanNotBeSaid';
+import { playerLogIn } from 'actions/authentication';
+import { useDispatch } from 'hooks/useDispatch';
+import { useTypedSelector } from 'hooks/useTypedSelector';
+
+export const Lobby: React.FC = () => {
+	const dispatch = useDispatch();
+
+	const playerAuth = useTypedSelector((state) => state.entities.playerAuth);
+	const joinRoom = () => {
+		dispatch(playerLogIn(playerAuth));
+	};
+
+	return (
+		<>
+			<H1>Hello World! It's fika time!</H1>
+			<Row>
+				<Column>
+					<TextField
+						borderColor="gray"
+						required
+						entityKey="playerAuth.playerName"
+						title="Player name"
+						validators={[validateRequiredField]}
+					/>
+				</Column>
+			</Row>
+			<Row>
+				<Column>
+					<Validate entities="playerAuth" onClick={joinRoom}>
+						<PrimaryButton testID="join-room">Join room</PrimaryButton>
+					</Validate>
+				</Column>
+			</Row>
+		</>
+	);
+};
